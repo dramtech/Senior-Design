0b6f93b675dc5b7c52a41066917f24c6b501f869
RealMackay <55122906+RealMackay@users.noreply.github.com>
2020-06-25 14:23:07 -0400

    Updated

--- MainMCU_Program/main.c
+++ MainMCU_Program/main.c
@@ -48,16 +48,27 @@
 
 // Car warning position
 #define RIGHT_CAR_POS_xMIN 107
+#define LEFT_CAR_POS_xMIN 0
+
+// Car detection distance
+#define DETECT_DIST 30
+
+// Road image position
+#define ROAD_POS_xMIN 31
+#define ROAD_POS_yMIN 9
 
 // Temperature position
 #define TEMP_TEXT_POS_yMIN (LCD_Y_SIZE - 10)
 
 uint8_t togg = 0;
-uint8_t flag = 0;
+uint8_t flag_L = 0;
+uint8_t flag_R = 0;
 uint8_t measure_dis = 0;
 uint8_t temp_flag = 20;
 uint8_t angle_flag = 0;
 uint8_t bluetooth_conn_flag = 0;
+uint8_t angle_threshold_flag = 0;
+uint8_t threshold_counter = 0;
 
 #if defined(__IAR_SYSTEMS_ICC__)
 int16_t __low_level_init(void) {
@@ -95,21 +106,27 @@ void floatToString(float input, char * output) {
 __interrupt void T0A0_ISR() {
     // Toggle the LEDs for debugging
     P1OUT ^= redLED;
-    TA0CCR0 += ONE_SEC / 2; // Schedule the next interrupt
+    TA0CCR0 += 16384; // Schedule the next interrupt
 
     togg ^= BIT0;
     // Hardware clears the flag (CCIFG in TA0CCTL0)
 
-    if(togg == 0)
-        flag = 1;
+    if(togg == 0) {
+        flag_L = 1;
+        flag_R = 1;
+    }
 
     if(togg == 0x01) {
-        flag = 2;
+        flag_L = 2;
+        flag_R = 2;
         temp_flag++;
         angle_flag++;
         measure_dis = 1;
     }
 
+    if (angle_threshold_flag) {
+        threshold_counter++;
+    }
 }
 
 int main(void)
@@ -177,6 +194,12 @@ int main(void)
 
     Graphics_clearDisplay(&g_sContext);
 
+    // Drae road
+    Graphics_drawImage(&g_sContext,
+                       &road1BPP_COMP_RLE4,
+                       ROAD_POS_xMIN,
+                       ROAD_POS_yMIN);
+
     // Draw right car warning image
     Graphics_drawImage(&g_sContext,
                        &rightCarIndicator1BPP_COMP_RLE4,
@@ -185,23 +208,29 @@ int main(void)
 
     // Text for debugging
     Graphics_drawString(&g_sContext, "Temperature: ", strlen("Temperature: "), 0, TEMP_TEXT_POS_yMIN, GRAPHICS_TRANSPARENT_TEXT);
-    Graphics_drawString(&g_sContext, "Angle: ", strlen("Angle: "), 0, TEMP_TEXT_POS_yMIN - 15, GRAPHICS_TRANSPARENT_TEXT);
+//    Graphics_drawString(&g_sContext, "Angle: ", strlen("Angle: "), 0, TEMP_TEXT_POS_yMIN - 15, GRAPHICS_TRANSPARENT_TEXT);
 
-    Graphics_Rectangle rect, rect2, temp_del, angle_rect;
+    Graphics_Rectangle rect_R, rect_L, rect2, temp_del, angle_rect;
 
-    // Distance deleting square
-    rect.xMin = RIGHT_CAR_POS_xMIN;
-    rect.xMax = LCD_X_SIZE;
-    rect.yMin = LCD_Y_SIZE / 2 - 10;
-    rect.yMax = LCD_Y_SIZE / 2 + 10;
+    // RIGHT car deleting square
+    rect_R.xMin = RIGHT_CAR_POS_xMIN;
+    rect_R.xMax = LCD_X_SIZE;
+    rect_R.yMin = LCD_Y_SIZE / 2 - 10;
+    rect_R.yMax = LCD_Y_SIZE / 2 + 10;
 
-    // Car warning deleting square
+    // LEFT car deleting square
+    rect_L.xMin = LEFT_CAR_POS_xMIN;
+    rect_L.xMax = LEFT_CAR_POS_xMIN + 20;
+    rect_L.yMin = LCD_Y_SIZE / 2 - 10;
+    rect_L.yMax = LCD_Y_SIZE / 2 + 10;
+
+    // Car warning deleting square (debugging)
     rect2.xMin = 0;
     rect2.xMax = Graphics_getStringWidth(&g_sContext, distance_str, -1);
     rect2.yMin = 0;
     rect2.yMax = Graphics_getStringHeight(&g_sContext);
 
-    // Temperature deleting square
+    // Temperature deleting square (debugging)
     temp_del.xMin = Graphics_getStringWidth(&g_sContext, "Temperature: ", -1);
     temp_del.xMax = Graphics_getStringWidth(&g_sContext, "Temperature: ", -1)
                     + Graphics_getStringWidth(&g_sContext, temp_str, -1);
@@ -218,9 +247,10 @@ int main(void)
     init_timer();
     init_bluetooth();
 
-    _enable_interrupts();
+    _disable_interrupts();
 
     while(1) {
+
         // TODO Bluetooth procedure
         if(procedure[0]) {
             // TODO implement all Bluetooth interaction inside this if statement
@@ -244,6 +274,9 @@ int main(void)
                     procedure[3] = ON; //Gyro/Acc
 
                     bluetooth_pair_flag = 1; // Set bluetooth pair ON.
+                    temp_flag = 15; // Set temp_flag to output temperature value immediately after bluetooth is paired.
+                    angle_flag = 0;
+                    _enable_interrupts();
                 }
             }
         }
@@ -261,23 +294,43 @@ int main(void)
             }
 
             // Right car warning
-            if(distance_cm[0] < 220) {
-                if(flag == 1) {
-                    flag = 0;
+            if(distance_cm[0] < DETECT_DIST) {
+                if(flag_R == 1) {
+                    flag_R = 0;
                     Graphics_setForegroundColor(&g_sContext, ClrWhite);
                     Graphics_drawImage(&g_sContext,
                                        &rightCarIndicator1BPP_COMP_RLE4,
                                        RIGHT_CAR_POS_xMIN,
                                        LCD_Y_SIZE / 2 - 10);
                 }
-                if(flag == 2) {
-                    flag = 0;
+                if(flag_R == 2) {
+                    flag_R = 0;
                     Graphics_setForegroundColor(&g_sContext, ClrBlack);
-                    Graphics_fillRectangle(&g_sContext, &rect);
+                    Graphics_fillRectangle(&g_sContext, &rect_R);
                 }
-            }else {
+            } else {
                 Graphics_setForegroundColor(&g_sContext, ClrBlack);
-                Graphics_fillRectangle(&g_sContext, &rect);
+                Graphics_fillRectangle(&g_sContext, &rect_R);
+            }
+
+            // Left car warning
+            if(distance_cm[1] < DETECT_DIST) {
+                if(flag_L == 1) {
+                    flag_L = 0;
+                    Graphics_setForegroundColor(&g_sContext, ClrWhite);
+                    Graphics_drawImage(&g_sContext,
+                                       &leftCarIndicator1BPP_COMP_RLE4,
+                                       LEFT_CAR_POS_xMIN,
+                                       LCD_Y_SIZE / 2 - 10);
+                }
+                if(flag_L == 2) {
+                    flag_L = 0;
+                    Graphics_setForegroundColor(&g_sContext, ClrBlack);
+                    Graphics_fillRectangle(&g_sContext, &rect_L);
+                }
+            } else {
+                Graphics_setForegroundColor(&g_sContext, ClrBlack);
+                Graphics_fillRectangle(&g_sContext, &rect_L);
             }
 
             // TODO left car warning - need second US sensor
@@ -315,20 +368,38 @@ int main(void)
             if(angle_flag == 2) {
                 angle_flag = 0;
                 getAngle(&angle);
-//                floatToString(angle, angle_str);
-//
-//                // Print to the screen for debugging
-//                Graphics_setForegroundColor(&g_sContext, ClrBlack);
-//                Graphics_fillRectangle(&g_sContext, &angle_rect);
-//                Graphics_setForegroundColor(&g_sContext, ClrWhite);
-//                angle_rect.xMax = Graphics_getStringWidth(&g_sContext, "Angle: ", -1)
-//                                  + Graphics_getStringWidth(&g_sContext, angle_str, -1);
-//
-//                Graphics_drawString(&g_sContext,
-//                                    angle_str, strlen(angle_str),
-//                                    Graphics_getStringWidth(&g_sContext, "Angle: ", -1),
-//                                    TEMP_TEXT_POS_yMIN - 15,
-//                                    GRAPHICS_TRANSPARENT_TEXT);
+
+                float threshold = 60.0;
+
+                if (angle > threshold) {
+                    angle_threshold_flag = 1;
+                } else {
+                    angle_threshold_flag = 0;
+                    threshold_counter = 0;
+                }
+
+                if (threshold_counter > 9) {
+                    threshold_counter = 0;
+                    angle_threshold_flag = 0;
+
+                    unsigned char test[] = {'P', '\0'};
+                    transmit_data(test);
+                }
+
+                floatToString(angle, angle_str);
+
+                // Print to the screen for debugging
+                Graphics_setForegroundColor(&g_sContext, ClrBlack);
+                Graphics_fillRectangle(&g_sContext, &angle_rect);
+                Graphics_setForegroundColor(&g_sContext, ClrWhite);
+                angle_rect.xMax = Graphics_getStringWidth(&g_sContext, "Angle: ", -1)
+                                  + Graphics_getStringWidth(&g_sContext, angle_str, -1);
+
+                Graphics_drawString(&g_sContext,
+                                    angle_str, strlen(angle_str),
+                                    Graphics_getStringWidth(&g_sContext, "Angle: ", -1),
+                                    TEMP_TEXT_POS_yMIN - 15,
+                                    GRAPHICS_TRANSPARENT_TEXT);
 
             }
         }
